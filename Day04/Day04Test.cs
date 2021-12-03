2599d4f34434bb32e379832f375967247f31f999
Nico Zink <nico@zink.dev>
2021-12-03 21:55:01 +0000

    Day 4 prep

---
+++ Day04/Day04Test.cs
@@ -0,0 +1,35 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions;
+
+namespace Tests
+{
+    [TestClass]
+    public class Day04Test
+    {
+        private readonly Day04Solution solver = new ();
+
+        [TestMethod]
+        public void TestExample1()
+        {
+            Assert.AreEqual(0, solver.SolveExample1());
+        }
+
+        [TestMethod]
+        public void TestSolution1()
+        {
+            Assert.AreEqual(0, solver.SolvePart1());
+        }
+
+        [TestMethod]
+        public void TestExample2()
+        {
+            Assert.AreEqual(0, solver.SolveExample2());
+        }
+
+        [TestMethod]
+        public void TestSolution2()
+        {
+            Assert.AreEqual(0, solver.SolvePart2());
+        }
+    }
+}
