30dd02fa8cd172437acc2b319f089cd9223574e2
Nico Zink <nico@zink.dev>
2021-12-04 17:46:38 +0000

    Day 4 solution

--- Day04/Day04Test.cs
+++ Day04/Day04Test.cs
@@ -11,25 +11,25 @@ namespace Tests
         [TestMethod]
         public void TestExample1()
         {
-            Assert.AreEqual(0, solver.SolveExample1());
+            Assert.AreEqual(4512, solver.SolveExample1());
         }
 
         [TestMethod]
         public void TestSolution1()
         {
-            Assert.AreEqual(0, solver.SolvePart1());
+            Assert.AreEqual(33462, solver.SolvePart1());
         }
 
         [TestMethod]
         public void TestExample2()
         {
-            Assert.AreEqual(0, solver.SolveExample2());
+            Assert.AreEqual(1924, solver.SolveExample2());
         }
 
         [TestMethod]
         public void TestSolution2()
         {
-            Assert.AreEqual(0, solver.SolvePart2());
+            Assert.AreEqual(30070, solver.SolvePart2());
         }
     }
 }
