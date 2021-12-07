76576371d3d090318d2a27235af5bdaf94967237
nicozink <nico@nfzink.com>
2021-12-07 23:26:16 +0000

    Day 8 prep

---
+++ Day08/Day08Solution.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Common;
+
+namespace Solutions
+{
+    /// <summary>
+    /// Solution for day 8:
+    /// https://adventofcode.com/2021/day/8
+    /// </summary>
+    [SolutionClass(Day = 8)]
+    public class Day08Solution
+    {
+        #region Solve Problems
+
+        public long SolveExample1() => 0;
+
+        [SolutionMethod(Part = 1)]
+        public long SolvePart1() => 0;
+
+        public long SolveExample2() => 0;
+
+        [SolutionMethod(Part = 2)]
+        public long SolvePart2() => 0;
+
+        #endregion
+    }
+}
