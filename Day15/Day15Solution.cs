982be423dd2c9c19614fcc1867d7451729f571a4
Nico Zink <nico@zink.dev>
2021-12-15 10:43:47 +0000

    Day 15 prep

---
+++ Day15/Day15Solution.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Common;
+
+namespace Solutions
+{
+    /// <summary>
+    /// Solution for day 15:
+    /// https://adventofcode.com/2021/day/15
+    /// </summary>
+    [SolutionClass(Day = 15)]
+    public class Day15Solution
+    {
+        #region Solve Problems
+
+        public long SolveExample1() => 0;
+
+        [SolutionMethod(Part = 1)]
+        public long SolvePart1() => 0;
+
+        public long SolveExample2() => 0;
+
+        [SolutionMethod(Part = 2)]
+        public long SolvePart2() => 0;
+
+        #endregion
+    }
+}
