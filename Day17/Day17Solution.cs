fde122292dd93118a284a4a711eb9de87265e33a
nicozink <nico@nfzink.com>
2021-12-16 21:08:29 +0000

    Day 17 prep

---
+++ Day17/Day17Solution.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Common;
+using Solutions.Day11Utils;
+
+namespace Solutions
+{
+    /// <summary>
+    /// Solution for day 17:
+    /// https://adventofcode.com/2021/day/17
+    /// </summary>
+    [SolutionClass(Day = 17)]
+    public class Day17Solution
+    {
+        #region Solve Problems
+
+        public long SolveExample1() => 0;
+
+        [SolutionMethod(Part = 1)]
+        public long SolvePart1() => 0;
+
+        public long SolveExample2() => 0;
+
+        [SolutionMethod(Part = 2)]
+        public long SolvePart2() => 0;
+
+        #endregion
+    }
+}
---
+++ Day17/Day17Test.cs
@@ -0,0 +1,35 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions;
+
+namespace Tests
+{
+    [TestClass]
+    public class Day17Test
+    {
+        private readonly Day17Solution solver = new ();
+
+        [TestMethod]
+        public void TestExample1()
+        {
+            Assert.AreEqual(0, solver.SolveExample1());
+        }
+
+        [TestMethod]
+        public void TestSolution1()
+        {
+            Assert.AreEqual(0, solver.SolvePart1());
+        }
+
+        [TestMethod]
+        public void TestExample2()
+        {
+            Assert.AreEqual(0, solver.SolveExample2());
+        }
+
+        [TestMethod]
+        public void TestSolution2()
+        {
+            Assert.AreEqual(0, solver.SolvePart2());
+        }
+    }
+}
