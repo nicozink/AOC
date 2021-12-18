dfb61247a50671a777a74da3b9a183e33d0e8e69
Nico Zink <nico@zink.dev>
2021-12-18 17:35:43 +0000

    Day 17 & 18 solutions

--- Day18/Day18Test.cs
+++ Day18/Day18Test.cs
@@ -4,32 +4,30 @@ using Solutions;
 namespace Tests
 {
     [TestClass]
-    public class Day17Test
+    public class Day18Test
     {
-        private readonly Day17Solution solver = new ();
-
         [TestMethod]
         public void TestExample1()
         {
-            Assert.AreEqual(45, solver.SolveExample1());
+            Assert.AreEqual(4140, Day18Solution.SolveExample1());
         }
 
         [TestMethod]
         public void TestSolution1()
         {
-            Assert.AreEqual(15931, solver.SolvePart1());
+            Assert.AreEqual(4132, Day18Solution.SolvePart1());
         }
 
         [TestMethod]
         public void TestExample2()
         {
-            Assert.AreEqual(112, solver.SolveExample2());
+            Assert.AreEqual(3993, Day18Solution.SolveExample2());
         }
 
         [TestMethod]
         public void TestSolution2()
         {
-            Assert.AreEqual(2555, solver.SolvePart2());
+            Assert.AreEqual(4685, Day18Solution.SolvePart2());
         }
     }
 }
